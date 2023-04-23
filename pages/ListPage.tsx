4bfd65f57d291f743fce1d26d063ac44082f2f2b
RohanS14 <rohan@subramanianfamily.com>
2023-04-22 21:20:28 -0700

    added multiple courses

--- pages/ListPage.tsx
+++ pages/ListPage.tsx
@@ -28,12 +28,36 @@ export default function ListPage() {
       image: '/eng.jpg',
       pdf: "/public-docs/english/Williams Style 11th edition.pdf"
     },
-  ]);
+  ])
+  
+  const [courses, setCourses] = useState([
+    {
+      name: 'MATH062',
+      subject: 'Probability and Statistics',
+      folder: "/Chat?filePath=stats&title=OpenIntro Statistics",
+      image: '/math62.jpeg',
+      pdf: "public-docs/stats/Intro Stats.pdf"
+    },
+    {
+      name: 'WRIT001',
+      subject: 'Introduction to Academic Writing',
+      folder: "/Chat?filePath=english&title=Style: Lessons in Clarity and Grace",
+      image: '/writ1.png',
+      pdf: "/public-docs/english/Williams Style 11th edition.pdf"
+    },
+    {
+      name: 'CHEM024',
+      subject: 'Chemistry in the Modern World',
+      folder: "/Chat?filePath=chemistry&title=Elementary Principles of Chemical Processes",
+      image: '/chem24.png',
+      pdf: "/public-docs/stats/ElementaryChemical.pdf"
 
-  const renderListings = () => {
-    // render 10 of the first element in textbooks state
+    },
+    ])
 
-    return textbooks.map((textbook, index) => {
+  const renderListings = (data) => {
+
+    return data.map((textbook, index) => {
       return (
         <div key={index} className="">
           <Card
@@ -53,7 +77,27 @@ export default function ListPage() {
   return (
     <div className="flex h-screen flex-col">
       <Layout title="MuddBot">
+        
+        <h1 style={{ fontSize: "2.5rem" }}>My Textbooks</h1>
+        <br/>
+        <h2 style={{ fontSize: "1rem" }}>My College: Harvey Mudd</h2>
+        
         <div style={{ flex: 10, padding: 30 }}></div>
+        <div
+          className="flex flex-wrap space-x-2"
+          style={{
+            display: 'flex',
+            flex: 1,
+            justifyContent: 'center',
+            paddingRight: 50,
+            paddingLeft: 50,
+            paddingBottom: 50
+          }}
+        >
+          {renderListings(textbooks)}
+        </div>
+
+        <h1 style={{ fontSize: "2.5rem" }}>My Courses</h1>
         
         <div
           className="flex flex-wrap space-x-2"
@@ -66,8 +110,9 @@ export default function ListPage() {
             paddingBottom: 50
           }}
         >
-          {renderListings()}
+          {renderListings(courses)}
         </div>
+        
       </Layout>
     </div>
   );
